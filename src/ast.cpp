049e7d1a8103f712b75642168247a04f37804c87
Mr.UNIX <mrunix00@protonmail.com>
2024-06-18 18:47:46 +0100

    AST: add some assertions

--- src/ast.cpp
+++ src/ast.cpp
@@ -1,6 +1,12 @@
 #include "ast.h"
 
+#include <stdexcept>
 #include <utility>
+
+static inline void assert(bool condition, const char *message) {
+    if (!condition)
+        throw std::runtime_error("Assertion failed: " + std::string(message));
+}
 
 Node::Node(Token token) : token(std::move(token)) {
     nodeType = Type::Node;
@@ -13,6 +19,8 @@ bool Node::operator==(const AbstractSyntaxTree &other) const {
 BinaryExpression::BinaryExpression(AbstractSyntaxTree *left, AbstractSyntaxTree *right, Token op)
     : left(left), right(right), op(std::move(op)) {
     nodeType = Type::BinaryExpression;
+    assert(left != nullptr, "Left operand can't be null!");
+    assert(right != nullptr, "Right operand can't be null!");
 }
 bool BinaryExpression::operator==(const AbstractSyntaxTree &other) const {
     if (other.nodeType != nodeType) return false;
@@ -25,10 +33,13 @@ bool BinaryExpression::operator==(const AbstractSyntaxTree &other) const {
 Declaration::Declaration(AbstractSyntaxTree *type, Node identifier, AbstractSyntaxTree *value)
     : type(type), identifier(std::move(identifier)), value(value) {
     nodeType = Type::Declaration;
+    assert(value != nullptr, "Initialization can't be null!");
+    assert(type != nullptr, "Type can't be null!");
 }
 Declaration::Declaration(AbstractSyntaxTree *type, Node identifier)
     : type(type), identifier(std::move(identifier)) {
     nodeType = Type::Declaration;
+    assert(type != nullptr, "Type can't be null!");
 }
 bool Declaration::operator==(const AbstractSyntaxTree &other) const {
     if (other.nodeType != nodeType) return false;
@@ -60,6 +71,7 @@ bool ScopedBody::operator==(const AbstractSyntaxTree &other) const {
 FunctionDeclaration::FunctionDeclaration(AbstractSyntaxTree *returnType, const std::vector<Declaration *> &arguments)
     : returnType(returnType), arguments(arguments) {
     nodeType = Type::FunctionDeclaration;
+    assert(returnType != nullptr, "Return type can't be null!");
 }
 bool FunctionDeclaration::operator==(const AbstractSyntaxTree &other) const {
     if (other.nodeType != nodeType) return false;
@@ -79,6 +91,7 @@ bool FunctionDeclaration::operator==(const AbstractSyntaxTree &other) const {
 ReturnStatement::ReturnStatement(AbstractSyntaxTree *expression)
     : expression(expression) {
     nodeType = Type::ReturnStatement;
+    assert(expression != nullptr, "Return expression can't be null!");
 }
 
 bool ReturnStatement::operator==(const AbstractSyntaxTree &other) const {

c6d2d015aea3b0c86b85bc8797734d18eaf01679
Mr.UNIX <mrunix00@protonmail.com>
2024-06-24 11:21:07 +0100

    main.cpp: allow SPL to read code from files

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
+#include "ast.h"
+#include "vm.h"
+#include <fstream>
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <input>" << std::endl;
+        return EXIT_FAILURE;
+    }
+    std::ifstream file(argv[1]);
+    if (!file.is_open()) {
+        std::cerr << "Failed to open file: " << argv[1] << std::endl;
+        return EXIT_FAILURE;
+    }
+    std::string input((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    file.close();
+    VM vm;
+    auto program = compile(input.c_str());
+    vm.run(program);
+    std::cout << *static_cast<int32_t *>(vm.popStack(sizeof(int32_t))) << std::endl;
+    return EXIT_SUCCESS;
 }
